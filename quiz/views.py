6537af27a26a6bd6812067530e9d3c3f9f341ea0
lisy0123 <leesese3320@gmail.com>
2020-11-22 20:08:10 +0900

    Study flutter django heroku

---
+++ quiz/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Quiz
+from .serializers import QUizSerializer
+import random
+
+# Create your views here.
+@api_view(['GET'])
+def helloAPI(request):
+    return Response("hello world!")
+
+@api_view(['GET'])
+def randomQuiz(request, id):
+    totalQuizs = Quiz.objects.all()
+    randomQuizs = random.sample(list(totalQuizs), id)
+    serializer = QUizSerializer(randomQuizs, many = True)
+    return Response(serializer.data)
